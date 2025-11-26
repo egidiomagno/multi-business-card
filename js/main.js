06c7e24ff0ab041c5493375ef733bcc6e36fdcc3
egidiomagno <egidio.magno107@gmail.com>
2025-11-26 11:47:39 +0900

    Ok

--- js/main.js
+++ js/main.js
@@ -13,21 +13,19 @@ fetch('data/cards.json')
           <div class="card">
             <div class="header">
               <div class="pattern"></div>
-              <img src="${card.photo}" alt="Photo" class="photo">
+              <img src="${card.photo}" alt="Photo" class="photo" style="width:100px; height:100px; border-radius:50%;">
             </div>
             <div class="body">
-              <div>
-                <h1 class="name">${card.name}</h1>
-                <p class="title">${card.title}</p>
-                <p class="organization">${card.organization}</p>
-                <p class="address">${card.address}</p>
-                <p class="contact">
-                  T: ${card.phone} | P: ${card.phone2} <br>
-                  E: ${card.email} <br>
-                  W: ${card.web}
-                </p>
-              </div>
-              <img src="${card.qr}" alt="QR Code" class="qr">
+              <h1 class="name">${card.name}</h1>
+              <p class="title">${card.title}</p>
+              <p class="organization">${card.organization}</p>
+              <p class="address">${card.address}</p>
+              <p class="contact">
+                T: ${card.phone} | P: ${card.phone2} <br>
+                E: ${card.email} <br>
+                W: ${card.web}
+              </p>
+              <img src="${card.qr}" alt="QR Code" class="qr" style="width:120px; height:120px;">
             </div>
           </div>
         `;
